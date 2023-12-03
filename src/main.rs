58cc365bf2200421995033f672e11e68dd617f8f
Duane Littleton <dlittleton87@gmail.com>
2023-12-03 10:02:29 -0500

    Day 1, part 2

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
 
     let lines = read_input_file(input_file);
     day_1(&lines);
+    day_1_1(&lines);
 }
 
 fn read_input_file(path: &String) -> Vec<String> {
@@ -40,6 +41,44 @@ fn day_1(lines: &Vec<String>) {
         let numeric = digits.parse::<i32>().unwrap();
 
         total += numeric;
+    }
+
+    println!("{}", total);
+}
+
+fn day_1_1(lines: &Vec<String>) {
+    let re = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine|zero)").unwrap();
+
+    let mut total = 0;
+    for l in lines {
+        let mut digits: Vec<i32> = vec![];
+        let mut index = 0;
+        while let Some(m) = re.find_at(l, index) {
+            index = m.start() + 1;
+            let capture = m.as_str();
+            let value = match capture {
+                "one" => 1,
+                "two" => 2,
+                "three" => 3,
+                "four" => 4,
+                "five" => 5,
+                "six" => 6,
+                "seven" => 7,
+                "eight" => 8,
+                "nine" => 9,
+                "zero" => 0,
+                _ => capture.parse::<i32>().unwrap(),
+            };
+
+            digits.push(value);
+        }
+
+        let first_digit = digits.first().unwrap();
+        let second_digit = digits.last().unwrap();
+
+        info!("{}{}", first_digit, second_digit);
+
+        total += (10 * first_digit) + second_digit;
     }
 
     println!("{}", total);
