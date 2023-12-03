3672573850a45d55bd421885308a7e577157f265
Duane Littleton <dlittleton87@gmail.com>
2023-12-02 22:56:11 -0500

    Add log crate
    Initial setup to read arguments and log.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use log::{debug, info};
+use std::env;
+use std::fs;
+
 fn main() {
+    env_logger::init();
     println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    let day = &args[1];
+    let input_file = &args[2];
+
+    info!("Day {}, Files {}", day, input_file);
+
+    let contents = fs::read_to_string(input_file)
+        .unwrap_or_else(|_| panic!("Unable to open input file: {}", input_file));
+
+    debug!("Input contents:\n{}", contents);
 }
