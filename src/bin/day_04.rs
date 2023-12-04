8875ca01a490da69b194ed20b7d034352ad1e42d
Duane Littleton <dlittleton87@gmail.com>
2023-12-04 16:00:45 -0500

    Move parsing into separate function

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -5,18 +5,25 @@ use std::collections::HashSet;
 fn part1(lines: &[String]) -> String {
     let mut total = 0;
     for line in lines {
-        let (_, ticket) = line.split_once(':').unwrap();
-        let (winning, have) = ticket.split_once('|').unwrap();
+        let (_, card) = line.split_once(':').unwrap();
 
-        let winning_set: HashSet<i32> = HashSet::from_iter(util::get_all_numbers(winning));
-        let have_set: HashSet<i32> = HashSet::from_iter(util::get_all_numbers(have));
-
-        let nums = winning_set.intersection(&have_set).count();
-
-        if nums > 0 {
-            total += 2_i32.pow((nums - 1).try_into().unwrap())
-        }
+        total += count_winning_numbers(card);
     }
 
     format!("{}", total)
+}
+
+fn count_winning_numbers(card: &str) -> i32 {
+    let (winning, have) = card.split_once('|').unwrap();
+
+    let winning_set: HashSet<i32> = HashSet::from_iter(util::get_all_numbers(winning));
+    let have_set: HashSet<i32> = HashSet::from_iter(util::get_all_numbers(have));
+
+    let nums = winning_set.intersection(&have_set).count();
+
+    if nums == 0 {
+        0
+    } else {
+        2_i32.pow((nums - 1).try_into().unwrap())
+    }
 }
