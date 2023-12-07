3364d5e3c57ee915084df09889207af117ae9657
Duane Littleton <dlittleton87@gmail.com>
2023-12-06 22:08:48 -0500

    Day 6, part 2

--- src/bin/day_06.rs
+++ src/bin/day_06.rs
@@ -1,6 +1,7 @@
-use aoc2023::util::get_all_numbers;
+use aoc2023::util::{get_all_numbers, get_first_number};
+use log::info;
 
-aoc2023::solver!(part1);
+aoc2023::solver!(part1, part2);
 
 fn part1(lines: &[String]) -> String {
     let times: Vec<i32> = get_all_numbers(&lines[0]);
@@ -26,4 +27,21 @@ fn part1(lines: &[String]) -> String {
     let result: i32 = wins.iter().product();
 
     format!("{}", result)
+}
+
+fn part2(lines: &[String]) -> String {
+    let time: i64 = get_first_number(&lines[0].replace(" ", ""));
+    let distance = get_first_number(&lines[1].replace(" ", ""));
+
+    info!("Time: {}", time);
+    info!("Distance: {}", distance);
+
+    let mut count = 0;
+    for x in 0..time {
+        if (time - x) * x > distance {
+            count += 1;
+        }
+    }
+
+    format!("{}", count)
 }
