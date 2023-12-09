281a3d505ad408c21e8b33183370e3bc07d06c53
Duane Littleton <dlittleton87@gmail.com>
2023-12-09 15:24:25 -0500

    Day 9, part 2

--- src/bin/day_09.rs
+++ src/bin/day_09.rs
@@ -1,7 +1,7 @@
 use aoc2023::util::get_all_numbers;
 use log::info;
 
-aoc2023::solver!(part1);
+aoc2023::solver!(part1, part2);
 
 fn find_next_number(values: &[i32]) -> i32 {
     let diff: Vec<_> = values[..]
@@ -25,6 +25,34 @@ fn part1(lines: &[String]) -> String {
         .map(|line| {
             let values = get_all_numbers(line);
             find_next_number(&values)
+        })
+        .sum();
+
+    format!("{}", total)
+}
+
+fn find_previous_number(values: &[i32]) -> i32 {
+    let diff: Vec<_> = values[..]
+        .iter()
+        .zip(values[1..].iter())
+        .map(|(a, b)| b - a)
+        .collect();
+
+    info!("{:?}", diff);
+
+    if diff.iter().all(|v| *v == 0) {
+        return *values.first().unwrap();
+    } else {
+        return values.first().unwrap() - find_previous_number(&diff);
+    }
+}
+
+fn part2(lines: &[String]) -> String {
+    let total: i32 = lines
+        .iter()
+        .map(|line| {
+            let values = get_all_numbers(line);
+            find_previous_number(&values)
         })
         .sum();
 
